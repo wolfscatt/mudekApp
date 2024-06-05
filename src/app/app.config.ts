79c192728329647c0cf249ac28111600bc19bc65
wolfscatt <bjkeses11@hotmail.com>
2024-06-05 09:15:32 +0300

    Nav Updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { environment } from '../env/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../env/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
